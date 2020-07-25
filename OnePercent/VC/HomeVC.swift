e8cf8a9b182e3573240f7b24e5d7ebd346c836de
Shamik Patro <patroshamik@gmail.com>
2020-07-25 13:35:20 -0500

    Initial Commit

---
+++ OnePercent/VC/HomeVC.swift
@@ -0,0 +1,31 @@
+//
+//  HomeVC.swift
+//  OnePercent
+//
+//  Created by Shamik Patro on 7/25/20.
+//  Copyright © 2020 Astronomical. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = #colorLiteral(red: 0.968627451, green: 0.7960784314, blue: 0.7960784314, alpha: 1)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ OnePercent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnePercent
+//
+//  Created by Shamik Patro on 7/25/20.
+//  Copyright © 2020 Astronomical. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
