a1797acaada7333f138bcc76ad930303c64a309e
Shamik Patro <patroshamik@gmail.com>
2020-07-26 01:32:57 -0500

    Cut out more BS

--- OnePercent/VC/Main/HomeVC.swift
+++ OnePercent/VC/Main/HomeVC.swift
@@ -27,22 +27,12 @@ class HomeVC: UIViewController {
     
     private func configureNavBar(){
         
-        let sideMenuItem = UIBarButtonItem(image: UIImage(systemName: "line.horizontal.3"), style: .plain, target: self, action: #selector(pushSideMenuVC))
-        
-        let viewItem = UIBarButtonItem(image: UIImage(systemName: "square.grid.2x2"), style: .plain, target: self, action: #selector(changeView))
-        
         let image: UIImage = UIImage(named: "One Percent (transparent)")!
         let logo = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
         logo.contentMode = .scaleAspectFit
         logo.image = image
-        
-        navigationItem.setLeftBarButton(sideMenuItem, animated: true)
-        navigationItem.setRightBarButton(viewItem, animated: true)
-        navigationItem.leftBarButtonItem?.tintColor = #colorLiteral(red: 0.2156862745, green: 0.5058823529, blue: 0.168627451, alpha: 1)
-        navigationItem.rightBarButtonItem?.tintColor = #colorLiteral(red: 0.2170326445, green: 0.505137953, blue: 0.1686274558, alpha: 1)
             
         navigationItem.titleView = logo
-        
         
     }
     
@@ -72,22 +62,6 @@ class HomeVC: UIViewController {
         refreshController.addTarget(self, action: #selector(refreshView), for: .valueChanged)
         
         refreshController.tintColor = #colorLiteral(red: 0.2156862745, green: 0.5058823529, blue: 0.168627451, alpha: 1)
-        
-    }
-    
-    @objc func pushSideMenuVC(){
-        
-        let sideMenuVC = SideMenuVC()
-        
-        //navigationController?.pushViewController(sideMenuVC, animated: true)
-        
-    }
-    
-    @objc func changeView(){
-        
-        print("View Changed!")
-        
-        
         
     }
     
--- OnePercent/VC/Main/MerchVC.swift
+++ OnePercent/VC/Main/MerchVC.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class MerchVC: UIViewController {
+    
+    let merchView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,13 +18,10 @@ class MerchVC: UIViewController {
         view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
 
         configureNavBar()
+        configureMerchView()
     }
     
     private func configureNavBar(){
-        
-        let cartItem = UIBarButtonItem(image: UIImage(systemName: "square.grid.2x2"), style: .plain, target: self, action: #selector(pushCartVC))
-        
-        let sideMenuItem = UIBarButtonItem(image: UIImage(systemName: "line.horizontal.3"), style: .plain, target: self, action: #selector(pushSideMenuVC))
         
         let image: UIImage = UIImage(named: "One Percent (transparent)")!
         let logo = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
@@ -30,28 +29,25 @@ class MerchVC: UIViewController {
         logo.image = image
 
         navigationItem.titleView = logo
-        navigationItem.setLeftBarButton(sideMenuItem, animated: true)
-        navigationItem.setRightBarButton(cartItem, animated: true)
-        navigationItem.leftBarButtonItem?.tintColor = #colorLiteral(red: 0.2170326445, green: 0.505137953, blue: 0.1686274558, alpha: 1)
-        navigationItem.rightBarButtonItem?.tintColor = #colorLiteral(red: 0.2170326445, green: 0.505137953, blue: 0.1686274558, alpha: 1)
-        
         
     }
     
-    @objc func pushCartVC(){
+    private func configureMerchView(){
         
-        let cartVC = CartVC()
+        view.addSubview(merchView)
         
-        navigationController?.pushViewController(cartVC, animated: true)
+        merchView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint.activate([
+            
+            merchView.topAnchor.constraint(equalTo: view.topAnchor),
+            merchView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            merchView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            merchView.heightAnchor.constraint(equalTo: view.heightAnchor)
+            
+        ])
         
     }
     
-    @objc func pushSideMenuVC(){
-        
-        let sideMenuVC = SideMenuVC()
-        
-        //navigationController?.pushViewController(sideMenuVC, animated: true)
-        
-    }
-
 }
